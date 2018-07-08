1e14787e68fd4f895775ab8ee128da9af62a9894
geetika25 <geetika2598@gmail.com>
2018-07-07 22:12:52 -0700

    print date and time

---
+++ problem.py
@@ -0,0 +1,8 @@
+#write a program to print current date and time
+
+#import time module
+import time
+
+##dd/mm/yy format
+print("current date : "+time.strftime("%d-%m-%y"))
+print("curent time :  "+time.strftime("%x"))
