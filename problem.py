862b84a8af20d3f654a3dd1df819002c4a361a22
geetika25 <geetika2598@gmail.com>
2018-07-08 08:50:58 -0700

    general problems

--- problem.py
+++ problem.py
@@ -15,5 +15,15 @@ area=PI*radius*radius
 print("The area of the circle is : ",area)
 
 #3.Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them
+first_nam=input("enter your first nane : ")
+last_name=input("enter your last name :  ")
+print("hii"," ",last_name," ",first_nam)
+
 #4.Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers
+
+num=input("enter some commas separated numbers : ")
+list=num.split(",")
+tuple=tuple(list)
+print("list of num : ",list)
+print("tuple of num : ",tuple)
 
